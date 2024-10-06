7601120c8b8adba44c872c2719b78b19eb340cb5
JirayutP <6432022721@student.chula.ac.th>
2024-10-06 19:57:20 +0700

    week9

--- car-rental-app/src/components/CarPanel.tsx
+++ car-rental-app/src/components/CarPanel.tsx
@@ -1,10 +1,19 @@
 'use client'
-import Link from "next/link";
 import ProductCard from "./ProductCard";
-import { useReducer, useRef } from "react";
+import { useReducer, useState } from "react";
+import Link from "next/link";
+import { useRef , useEffect} from "react";
+import getCars from "@/libs/getCars";
 
 export default function CarPanel(){
-
+    const [carResponse, setCarResponse] = useState(null)
+    useEffect(()=>{
+        const fetchData = async ()=>{
+            const cars = await getCars()
+            setCarResponse(cars)
+        }
+        fetchData()
+    }, [])
     const countRef = useRef(0)
     const inputRef = useRef<HTMLInputElement>(null)
 
@@ -22,24 +31,26 @@ export default function CarPanel(){
     }
 
     const [compareList, dispatchCompare] = useReducer(compareReducer, new Set<string>())
-        
+
     /**
      * Mooc Data for Demontration Only   
-     */
         const mockCarRepo=[
             {cid:'001', name:'Honda Civic', image:'/img/civic.jpg'},
             {cid:'002', name:'Honda Accord', image:'/img/accord.jpg'},
             {cid:'003', name:'Toyota Fortuner', image:'/img/fortuner.jpg'},
             {cid:'004', name:'Tesla Model 3', image:'/img/tesla.jpg'},
         ]
+     */
+
+    if(!carResponse) return (<p>Car Panel is Loading ...</p>)
 
     return(
         <div>
-            <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
+            <div style={{margin:"20px", display:"flex", flexDirection:"row", flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around", padding:'10px'}}>
                 {
-                    mockCarRepo.map((carItem)=>(
-                        <Link href={`/car/${carItem.cid}`} className="w-1/5">
-                            <ProductCard carName={carItem.name} imgSrc={carItem.image}
+                    carResponse.data.map((carItem:Object)=>(
+                        <Link href={`/car/${carItem.id}`} className="w-1/5">
+                            <ProductCard carName={carItem.model} imgSrc={carItem.picture}
                             onCompare={(car:string)=>dispatchCompare({type:'add', carName:car})}
                             />
                         </Link>
@@ -51,14 +62,15 @@ export default function CarPanel(){
             {Array.from(compareList).map((car)=><div key={car} 
                 onClick={()=>dispatchCompare({type:'remove', carName:car})}>
                 {car}</div>)}
-                <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
             onClick={()=>{countRef.current=countRef.current+1; alert(countRef.current)}}>
                 Count with Ref Object
             </button>
             <input type="text" placeholder="Please fill" className="block text-gray-900 text-sm rounded-lg
-                p-2 m-2 bg-purple-50 ring-1 ring-inset ring-purple-400
-                focus:outline-none focus:bg-purple-200 focus:ring-2"
-                ref={inputRef}/>
+            p-2 m-2 bg-purple-50 ring-1 ring-inset ring-purple-400
+            focus:outline-none focus:bg-purple-200 focus:ring-2"
+            ref={inputRef}/>
             <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 px-3 py-2 text-white shadow-sm"
             onClick={()=>{if(inputRef.current!=null) inputRef.current.focus()}}>
                 Focus Input
